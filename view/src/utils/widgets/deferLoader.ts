d74cc90cff4fca56da925037461a0e1eb01a8aed
ShawnPhang <palxiao@vip.qq.com>
2022-11-25 20:46:44 +0800

    build

--- view/src/utils/widgets/deferLoader.ts
+++ view/src/utils/widgets/deferLoader.ts
@@ -1,12 +1,15 @@
-
-
 export default (type: string, url: string) => {
-  const link_element = document.createElement(type)
-  if (type === 'script') {
-    link_element.setAttribute('src', url)
-  } else if (type === 'link') {
-    link_element.setAttribute('rel', 'stylesheet')
-    link_element.setAttribute('href', url)
-  }
-  document.head.appendChild(link_element)
+  return new Promise<void>((resolve) => {
+    const link_element = document.createElement(type)
+    if (type === 'script') {
+      link_element.setAttribute('src', url)
+    } else if (type === 'link') {
+      link_element.setAttribute('rel', 'stylesheet')
+      link_element.setAttribute('href', url)
+    }
+    document.head.appendChild(link_element)
+    link_element.onload = function () {
+      resolve()
+    }
+  })
 }
